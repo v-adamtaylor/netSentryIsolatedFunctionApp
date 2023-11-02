d58f9d2c5625e6354d5529dd5a69aa481e3d8455
v-adamtaylor <v-adamtaylor@microsoft.com>
2023-11-01 19:50:53 -0500

    Changed Function to Try Catch and to Emit Logs from Catch to Sentry.IO.

--- netSentryIsolated/Function1.cs
+++ netSentryIsolated/Function1.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Sentry;
 
 namespace netSentryIsolated
 {
@@ -20,15 +21,23 @@ namespace netSentryIsolated
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             // Create a task that tries to access an invalid path
-            Task faultyTask = Task.Run(() =>
+            Task faultyTask = Task.Run(async () =>
             {
                 try
                 {
-                    // This path is intentionally incorrect to simulate a network path not found exception
-                    File.ReadAllText(@"C:\home\LogFiles\Application\Functions\Host\NonExistentFile.txt");
+                    _logger.LogInformation("I've started my Task!");
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    // Simulate an exception
+                    throw new IOException("Simulated IO Exception: The network path was not found.");
                 }
-                catch (IOException ex) { throw new Exception("Could Not Find The File!!!", ex); }
-                
+                catch (Exception ex)
+                {
+                    _logger.LogError("An exception occurred in the faulty task.", ex);
+                    SentrySdk.CaptureException(ex); // Capture the exception manually
+                                                    // Rethrow if you want to preserve the original stack trace
+                                                    // and have it rethrown by the task scheduler
+                    throw;
+                }
             });
 
             // Do not await the task, causing it to be unobserved if it faults
@@ -44,6 +53,12 @@ namespace netSentryIsolated
 
             await response.WriteStringAsync("Function executed").ConfigureAwait(false);
 
+            _logger.LogInformation("The Function has completed and is returning!");
+
+            // Flush Sentry queue before exiting
+            await SentrySdk.FlushAsync(TimeSpan.FromSeconds(2));
+
+            //await Task.Delay(TimeSpan.FromSeconds(5)); // Give some time for the faultyTask to throw its exception
 
             return response;
         }
